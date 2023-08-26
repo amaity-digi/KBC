df4512572c0f3e3a691cfc38bcbb9c081230f6da
Ashis Maity <amaity@digisprint.com>
2023-08-26 09:45:35 +0530

    initial-commit

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import useSound from 'use-sound';
-
 
 export default function Timer({setStop,questionNumber}){
     const [timer,setTimer] =useState(30);
